00e6c3ef46aa3b6c5acae131e589251460ee234e
Matt Smith <matthew.smith491@gmail.com>
2016-09-02 13:09:27 -0700

    Switched to using reflection to get the object name

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,22 +1,16 @@
 package project_euler
 
+import scala.reflect.runtime.universe
+
 import org.rogach.scallop._
 
 object Main {
   def main(args: Array[String]) {
     val conf = new Conf(args)
-    conf.problem() match {
-      case 1 => Problem1
-      case 2 => Problem2
-      case 3 => Problem3
-      case 4 => Problem4
-      case 5 => Problem5
-      case 6 => Problem6
-      case 7 => Problem7
-      case 8 => Problem8
-      case 9 => Problem9
-      case _ => println("WHAAAAAAAT?")
-    }
+    val num = conf.problem()
+    val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
+    val module = runtimeMirror.staticModule(s"project_euler.Problem$num")
+    runtimeMirror.reflectModule(module).instance
   }
 }
 
